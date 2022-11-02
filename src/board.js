eec160139010884e088cb2214238440dd9b4f245
Natalia Rusu <bonapartik@gmail.com>
2022-11-02 20:31:40 +0000

    add id attribute to board item

--- src/board.js
+++ src/board.js
@@ -6,15 +6,18 @@
  * @returns div table with divs of rows and columns
  */
 
-const buildBoard=(rows, columns)=>{
+export const buildBoard=(rows, columns)=>{
     const boardEls=document.createElement('div');
     boardEls.classList.add('board-items');//will add grid class with 5 columns
+    let id=1;
     for(let i=0; i<rows; i++){
         const boardRowEl=document.createElement('div');//row
         boardRowEl.classList.add('board-row');
         for(let k=0; k<columns; k++){
             const rowItemEl=document.createElement('div');
             rowItemEl.classList.add('board-item');
+            rowItemEl.setAttribute('id', id);
+            id++;
             boardRowEl.append(rowItemEl);
         }
         boardEls.append(boardRowEl);
