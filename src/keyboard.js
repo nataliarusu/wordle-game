191b08cdc1a9a3d53d9cea8e0c8c1e9ec61657eb
Natalia Rusu <bonapartik@gmail.com>
2022-11-02 20:26:45 +0000

    remove event listener function, move it to index.js

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,28 +1,29 @@
-const keyboard_container=document.querySelector('.keyboard-layout');
+
 const keyboard_letters = [
   ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
   ['s', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
   ['Del', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Enter']
 ];
 
-for(const row of keyboard_letters){
-  const rowEl=document.createElement('div');
-  rowEl.classList.add('keybord-row')
-  for(let letter of row){
-    const btn=document.createElement('button');
-    btn.classList.add('keyboard-item');
-    btn.setAttribute('data-key', letter);
-    if(letter.length>1){
-      btn.classList.add('action');
+export const renderKeyboard=(keyboard_container)=>{
+  for(const row of keyboard_letters){
+    const rowEl=document.createElement('div');
+    rowEl.classList.add('keybord-row')
+    for(let letter of row){
+      const btn=document.createElement('button');
+      btn.classList.add('keyboard-item');
+      btn.setAttribute('data-key', letter);
+      if(letter.length>1){
+        btn.classList.add('action');
+      }
+      btn.innerHTML=letter;
+      rowEl.append(btn);
     }
-    btn.innerHTML=letter;
-    rowEl.append(btn);
+    keyboard_container.append(rowEl);
   }
-  keyboard_container.append(rowEl);
-}
-const pressKeyboardHandler=(ev)=>{
-  const letter=ev.target.textContent;
-  console.log(letter)
+  
 }
 
-keyboard_container.addEventListener('click', pressKeyboardHandler);
+
+
+
