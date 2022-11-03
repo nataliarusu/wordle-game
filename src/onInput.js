d2aa6dbc67c6a71d0e14608926aaaca6e0ac7eed
Natalia Rusu <bonapartik@gmail.com>
2022-11-03 16:20:49 +0000

    add func to check user input is letter or backspace

---
+++ src/onInput.js
@@ -0,0 +1,39 @@
+import { addLetter, removeLetter } from './addORremoveLetter.js';
+
+const isLetter = (value) => {
+  return /^[a-z]|[A-Z]$/.test(value);
+};
+
+
+let rowItems; //items in row
+let idx = 0; //to go to row dynamicly
+let currentId; //div board-item id
+
+export const updateParentElement = (row) => {
+  idx = 0;
+  rowItems = row.querySelectorAll('.board-item');
+  console.log(rowItems);
+};
+
+/**
+ *
+ * @param {Event key} key //user pressed key's value
+ */
+
+export const handleEnteredLetter = (key) => {
+  if (isLetter(key) && !currentWordCompleted) {
+    //currentWordCompleted is global 
+    currentId = rowItems[idx].id;
+    addLetter(key, document.getElementById(currentId), rowItems.length); //how many divs available in row
+    idx++;
+  } else if (key === 'Del' || key === 'Backspace') {
+    handleDeleteLetter();
+  }
+};
+
+const handleDeleteLetter = () => {
+  idx > 0 ? idx-- : (idx = 0);
+  currentId = rowItems[idx].id;
+  const currentBoardEl = document.getElementById(currentId);
+  removeLetter(currentBoardEl);
+};
