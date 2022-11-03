52c499177179eae8c0f9d0a2d0bfd0ee2814c897
Natalia Rusu <bonapartik@gmail.com>
2022-11-03 16:17:57 +0000

    add basic functionality

--- src/index.js
+++ src/index.js
@@ -1,59 +1,82 @@
 import { renderKeyboard } from './keyboard.js';
 import { buildBoard } from './board.js';
-import { addLetter, removeLetter } from './addLetter.js';
-const ATTEMPTS = 6;
-const MAX_W_LETTERS = 5;
-let currentId = 1;
+import { handleEnteredLetter, updateParentElement } from './onInput.js';
+import { getCurrentWord } from './addORremoveLetter.js';
+
+const MAX_ATTEMPTS = 9;
+const MAX_W_LETTERS = 8;
+const guessWords = []; //we will store arrays(user guess words) in array, so here will be 6 arrays
+let gameOver = false;
+globalThis.currentWordCompleted = false; //global available
+let currentParentEl; //current parent will be added later
+let idx; //helps to change currentParentEl depending on MAX_ATTEMPTS
+
 const boardLayout = document.querySelector('.board-layout');
 const keyboard_container = document.querySelector('.keyboard-layout');
 
-let gameOver = false;
-const board = buildBoard(ATTEMPTS, MAX_W_LETTERS);
-console.log(board);
+//build board depending on max values
+const board = buildBoard(MAX_ATTEMPTS, MAX_W_LETTERS);
 boardLayout.append(board);
 
-const isLetter = (value) => {
-  return /^[a-z]|[A-Z]$/.test(value);
+//only after board rendered we can get elements
+const rowEls = document.querySelectorAll('.board-row');
+
+const addWord = (word) => {
+  guessWords.push(word);
+  console.log(guessWords);
 };
 
-const customKeyboardHandler = (ev) => {
-   console.log(ev.target)
-  let key = ev.target.dataset.key;
-  let currentBoardEl = document.getElementById(currentId);
-  if (isLetter(key)) {
-    currentId ++;
-    addLetter(ev.target.textContent, currentBoardEl, MAX_W_LETTERS);
-  } else if (key === 'Del') {
-    currentId--;
-    currentBoardEl = document.getElementById(currentId);
-    removeLetter(currentBoardEl);
+const keyboardHandler = (ev) => {
+  let key;
+  if (ev instanceof KeyboardEvent) {
+    key = ev.key;
+  } else {
+    //Pointer event
+    console.log('pointer');
+    key = ev.target.dataset.key;
   }
-};
-const compKeyboardHandler = (ev) => {
-    console.dir(ev);
-  if (isLetter(ev.key)) {
-    console.log(ev.key);
-  }
-};
 
-const guessWords = [[]]; //we will store arrays(user guess words) in array, so here will be 6 arrays
+  if (currentWordCompleted && key === 'Enter') {
+    addWord(getCurrentWord());
+    //handle and check if match to today word
+    //check if game is over, riched all attempts
+    isGameOver();
+    //if not matched, if not over
+    idx++;
+    currentParentEl = rowEls[idx];
+    updateParentElement(currentParentEl); //to onInput
+
+    console.log(idx, currentParentEl, 'after row finished');
+
+    //if game is not over
+  } else {
+    console.log('handle here ' + currentWordCompleted);
+    handleEnteredLetter(key);
+  }
+};
 
 const isGameOver = () => {
   //check local storage
-  //if(gameOver)
-  if (guessWords.length < MAX_W_LETTERS) {
-    document.addEventListener('keyup', compKeyboardHandler);
-    keyboard_container.addEventListener('click', customKeyboardHandler);
-  } else {
-    document.removeEventListener('keyup');
-    keyboard_container.removeEventListener('click');
-    gameOver = true;
-  }
+  //if(gameOver) if 6 words in guessWords completed
+  /*if (guessWords.length > MAX_ATTEMPTS) {
+    console.log('game is over');
+    //handle the game result here
+    //gameOver = true;
+  
+  }*/
 };
 
 const startGame = () => {
+  idx = 0;
+  currentParentEl = rowEls[idx];
+  updateParentElement(currentParentEl); //to onInput
+  console.log(currentParentEl);
+  currentParentEl.classList.add('current');
   isGameOver();
   renderKeyboard(keyboard_container);
 };
 
 startGame();
+
+document.addEventListener('keyup', keyboardHandler);
+keyboard_container.addEventListener('click', keyboardHandler);
