195666c8dc57b052eb6d661d4d5f7e5eb5ea5287
Natalia Rusu <bonapartik@gmail.com>
2022-11-03 00:18:03 +0000

    add clearCurrentWord func which empties the array

--- src/addLetter.js
+++ src/addLetter.js
@@ -1,34 +1,35 @@
+const currentWord = [];
 
-const currentWord=[];
+export const addLetter = (letter, el, wlength) => {
+  console.log('add letter ', currentWord);
+  let complete = false;
+  if (currentWord.length < wlength) {
+    currentWord.push(letter);
+    el.setAttribute('data-value', letter);
+    el.innerHTML = letter.toUpperCase();
+  }
+  if (currentWord.length === 5) {
+    complete = true;
+  }
 
+  return complete;
+};
 
-export const addLetter=(letter, el, wlength)=>{
-    let complete=false;
-    if(currentWord.length<wlength){
-        currentWord.push(letter);
-        el.setAttribute('data-value', letter);
-        el.innerHTML=letter.toUpperCase();
-   
-    } 
-    if(currentWord.length===5){
-        complete=true;
-    }
+export const getCurrentWord = () => {
+  return currentWord;
+};
 
-   return complete;
-}
+export const clearCurrentWord = () => {
+  currentWord.length = 0; //empty array
+};
 
-export const getWord = ()=>{
-    return currentWord;
-}
+export const removeLetter = (el) => {
+  if (currentWord.length > 0) {
+    currentWord.pop();
+    el.removeAttribute('data-value');
+    console.log(el);
+    el.innerHTML = 'xxx';
+  }
 
-export const removeLetter=(el)=>{
-    if(currentWord.length>0){
-        currentWord.pop();
-        el.removeAttribute('data-value');
-        console.log(el)
-        el.innerHTML='xxx';
-        
-    } 
-   
-   console.log(currentWord)
-}
+  console.log(currentWord);
+};
