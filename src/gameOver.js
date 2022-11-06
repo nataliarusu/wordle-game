c29583d6eb2d5950eca527a85fb1d30c4bd9fe13
Natalia Rusu <bonapartik@gmail.com>
2022-11-06 23:44:54 +0000

    remove ev listeners when game is over

--- src/gameOver.js
+++ src/gameOver.js
@@ -1,9 +1,12 @@
-export const gameOver=(state, attempts)=>{
+export const gameOver=(state, attempts, keyboardHandler)=>{
     if(state ==='won'){
         console.log('you won in attepmts ', attempts)
+       
     } else if(state==='lost'){
         console.log('sorry you used all your ', attempts, ' attepmts')
     }
+    document.removeEventListener('keyup', keyboardHandler);
+    document.querySelector('.keyboard-layout').removeEventListener('click', keyboardHandler);
 
 //display attempts
 }
