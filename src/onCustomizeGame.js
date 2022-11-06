ef4d5855dad4226c439fc369ac5d3a11a73dcdea
Natalia Rusu <bonapartik@gmail.com>
2022-11-06 14:21:04 +0000

    add style to form
    Relates #6

--- src/onCustomizeGame.js
+++ src/onCustomizeGame.js
@@ -1,8 +1,10 @@
 export const customizeHandler=(ev)=>{
     const formContainer = document.querySelector('#custom-form');
     const closeBtn = formContainer.querySelector('.close-btn');
-    formContainer.classList.add('visible');  
+    formContainer.classList.add('visible');
+    formContainer.querySelector('.form-wrapper').classList.add('animated');  
     closeBtn.addEventListener('click', ()=>{
         document.querySelector('#custom-form').classList.remove('visible');
-    }) 
+        formContainer.querySelector('.form-wrapper').classList.remove('animated');  
+    }); 
 }
