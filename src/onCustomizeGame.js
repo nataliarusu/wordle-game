4e814f1fc7ea2864915b78c9ed1760edf25ad9a2
Natalia Rusu <bonapartik@gmail.com>
2022-11-06 11:11:44 +0000

    add style to form and form buttons

--- src/onCustomizeGame.js
+++ src/onCustomizeGame.js
@@ -1,5 +1,8 @@
 export const customizeHandler=(ev)=>{
     const formContainer = document.querySelector('#custom-form');
+    const closeBtn = formContainer.querySelector('.close-btn');
     formContainer.classList.add('visible');  
-      
+    closeBtn.addEventListener('click', ()=>{
+        document.querySelector('#custom-form').classList.remove('visible');
+    }) 
 }
